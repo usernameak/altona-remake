e6b0d36a321800be9556b42fe344253ae6d4bb48
UsernameAK <sitemakers.3dn.ru@gmail.com>
2017-09-24 12:30:10 +0300

    shit! revert!

--- base/system_linux.cpp
+++ base/system_linux.cpp
@@ -1740,6 +1740,10 @@ static void sXMessageLoop()
         }
 
         //sGetApp()->OnPrepareFrame();
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 3cb5b16... sBool -> bool
         if ((sSystemFlags & sISF_2D) && !sExitFlag)
         {
           Window root;
@@ -1765,6 +1769,7 @@ static void sXMessageLoop()
         }
         
         
+
         if (sSystemFlags & sISF_3D || sSystemFlags & sISF_CONTINUOUS)
           sUpdateWindow();
       //}
